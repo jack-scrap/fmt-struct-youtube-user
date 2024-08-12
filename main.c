6cbd0f3d7629c44cd191530a5ccff69563a31fb2
jackhasakeyboard <jackhasakeyboard@gmail.com>
2024-08-12 05:47:38 -0600

    Output input prefixed designating it as being a user

--- main.c
+++ main.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+
+const char prefix = '@';
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	printf("%c%s", prefix, argv[1]);
+
 	return 0;
 }
